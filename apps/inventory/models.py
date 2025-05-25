fe431a7c5527758bf5451b63371016d0f4fc74d4
aurechabnv <git@keltpoint.com>
2025-05-26 01:31:13 +0200

    Prevent stock to create unwanted movement when updated through a movement

--- apps/inventory/models.py
+++ apps/inventory/models.py
@@ -47,27 +47,33 @@ class Stock(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.IntegerField()
     last_modified = models.DateTimeField(auto_now=True)
+    synch = models.BooleanField(default=True)
 
     def __str__(self):
         return f"{self.product.sku} - {self.location.company.name} - {self.location.name} ({self.location.LocationType(self.location.type).label})"
 
     def save(self, *args, **kwargs):
-        # create movement in case of create/update
-        if self.id:
-            cur_obj = Stock.objects.get(pk=self.id)
-            delta = self.quantity - cur_obj.quantity
-        else:
-            delta = self.quantity
+        # Create a stock movement in case of create/update if synch is enabled
+        # Make sure to set the movement's attribute `synched` to True to prevent loop update Stock<>Movement
+        if self.synch:
+            if self.id:
+                cur_obj = Stock.objects.get(pk=self.id)
+                delta = self.quantity - cur_obj.quantity
+            else:
+                delta = self.quantity
 
-        is_inbound = delta > 0
-        movement_type = Movement.MovementType.INBOUND if is_inbound else Movement.MovementType.OUTBOUND
-        movement = Movement(type=movement_type, quantity=delta, product=self.product, synced=True)
+            is_inbound = delta > 0
+            movement_type = Movement.MovementType.INBOUND if is_inbound else Movement.MovementType.OUTBOUND
+            movement = Movement(type=movement_type, quantity=delta, product=self.product, synced=True)
 
-        if is_inbound:
-            movement.to_location = self.location
+            if is_inbound:
+                movement.to_location = self.location
+            else:
+                movement.from_location = self.location
+            movement.save()
         else:
-            movement.from_location = self.location
-        movement.save()
+            # If synch has been bypassed by a movement direct update, re-enable it
+            self.synch = True
 
         super().save(*args, **kwargs)
 
@@ -93,18 +99,22 @@ class Movement(models.Model):
         return f"{self.MovementType(self.type).label} du {self.date.strftime('%d-%m-%Y %H:%M')}"
 
     def save(self, *args, **kwargs):
+        # Make sure to only update stocks if movement is not synched yet
+        # Movement will be defaulted synched=True if it has been created from a stock update directly
         if not self.synced:
             self.synced = True
             # Get or create origin stock for OUTBOUND and TRANSFER
             if self.type != Movement.MovementType.INBOUND:
                 from_stock, created = Stock.objects.get_or_create(location=self.from_location, product=self.product, defaults={'quantity': 0})
                 from_stock.quantity -= self.quantity
+                from_stock.synch = False
                 from_stock.save()
 
             # Get or create destination stock for INBOUND and TRANSFER
             if self.type != Movement.MovementType.OUTBOUND:
                 to_stock, created = Stock.objects.get_or_create(location=self.to_location, product=self.product, defaults={'quantity': 0})
                 to_stock.quantity += self.quantity
+                to_stock.synch = False
                 to_stock.save()
 
         super().save(*args, **kwargs)
--- tests/unit/test_models.py
+++ tests/unit/test_models.py
@@ -6,6 +6,9 @@ from apps.inventory.models import Stock, Company, Movement, Location
 
 @pytest.mark.django_db
 def test_category():
+    """
+    Check that the category model works as expected.
+    """
     category = Category(name="Test Category")
     assert str(category) == "Test Category"
     assert category.slug == ""
@@ -15,6 +18,9 @@ def test_category():
 
 @pytest.mark.django_db
 def test_product():
+    """
+    Check that the product model works as expected.
+    """
     product = Product(name="Test Product", sku="test-product", price=15.0, stock_threshold=10)
     assert str(product) == "Test Product [test-product]"
     assert product.vat == 0.2
@@ -26,11 +32,17 @@ def test_product():
 
 @pytest.mark.django_db
 def test_company(company):
+    """
+    Check that the company model works as expected.
+    """
     assert str(company) == "Great Place for Computer Parts [12345678912345]"
 
 
 @pytest.mark.django_db
 def test_location(shop):
+    """
+    Check that the location model works as expected.
+    """
     assert str(shop) == "Great Place for Computer Parts - GPCP Montpellier"
     shop.siret = "32543442257446"
     assert str(shop) == "Great Place for Computer Parts - GPCP Montpellier [32543442257446]"
@@ -41,10 +53,14 @@ def test_location(shop):
 
 @pytest.mark.django_db
 def test_stock_creation(product2, warehouse):
+    """
+    Check that the stock creation works as expected.
+    """
     stock = Stock(location=warehouse, product=product2, quantity=35)
     assert str(stock) == "MY-OTHER-PRODUCT - Great Place for Computer Parts - Entrepot des tilleuls (Entrepôt)"
     assert stock.last_modified is None
     stock.save()
+    assert len(Movement.objects.all()) == 1
     latest_movement = Movement.objects.filter(product=product2).last()
     assert latest_movement.type == Movement.MovementType.INBOUND
     assert latest_movement.quantity == 35
@@ -53,11 +69,16 @@ def test_stock_creation(product2, warehouse):
 
 @pytest.mark.django_db
 def test_stock_update(stock_in_shop):
+    """
+    Check that the stock update works as expected.
+    """
     stock_modified_date = stock_in_shop.last_modified
     last_movement = Movement.objects.all().last()
     stock_in_shop.quantity -= 5
     stock_in_shop.save()
-    latest_movement = Movement.objects.all().last()
+    movements = Movement.objects.all()
+    assert movements.count() == 2
+    latest_movement = movements.last()
     assert stock_modified_date < stock_in_shop.last_modified
     assert last_movement.date < latest_movement.date
     assert latest_movement.type == Movement.MovementType.OUTBOUND
@@ -67,37 +88,58 @@ def test_stock_update(stock_in_shop):
 
 @pytest.mark.django_db
 def test_stock_outbound_updates(product1, shop, stock_in_shop, movement_outbound):
+    """
+    Check that the stock is properly updated on outbound and does not produce an additional movement item.
+    """
+    total_movements = Movement.objects.all().count()
     movement_outbound.synced = False
     movement_outbound.save()
+
     assert str(movement_outbound) == f"Sortie du {movement_outbound.date.strftime('%d-%m-%Y %H:%M')}"
     from_stock = Stock.objects.get(location=movement_outbound.from_location, product=movement_outbound.product)
     assert from_stock.quantity == 25
+    assert total_movements == Movement.objects.all().count()
 
 
 @pytest.mark.django_db
 def test_stock_inbound_updates(product1, warehouse, stock_in_warehouse, movement_inbound):
+    """
+    Check that the stock is properly updated on inbound and does not produce an additional movement item.
+    """
+    total_movements = Movement.objects.all().count()
     movement_inbound.synced = False
     movement_inbound.save()
+
     assert str(movement_inbound) == f"Entrée du {movement_inbound.date.strftime('%d-%m-%Y %H:%M')}"
     to_stock = Stock.objects.get(location=movement_inbound.to_location, product=movement_inbound.product)
     assert to_stock.quantity == 220
+    assert total_movements == Movement.objects.all().count()
 
 
 @pytest.mark.django_db
 def test_stock_transfer_updates(product1, warehouse, shop, stock_in_shop, stock_in_warehouse,
                                 movement_transfer):
+    """
+    Check that the stocks are properly updated on transfer and do not produce additional movement items.
+    """
+    total_movements = Movement.objects.all().count()
     movement_transfer.synced = False
     movement_transfer.save()
+
+    assert str(movement_transfer) == f"Transfert du {movement_transfer.date.strftime('%d-%m-%Y %H:%M')}"
     from_stock = Stock.objects.get(location=movement_transfer.from_location, product=movement_transfer.product)
     to_stock = Stock.objects.get(location=movement_transfer.to_location, product=movement_transfer.product)
-    assert str(movement_transfer) == f"Transfert du {movement_transfer.date.strftime('%d-%m-%Y %H:%M')}"
     assert from_stock.quantity == 100
     assert to_stock.quantity == 80
+    assert total_movements == Movement.objects.all().count()
 
 
 @pytest.mark.django_db
 def test_product_deletion(product1, stock_in_warehouse, stock_in_shop,
                           movement_inbound, movement_outbound, movement_transfer):
+    """
+    Check that the product deletion cascades properly.
+    """
     total, items = product1.delete()
     assert total == 10
     assert items['catalog.Product'] == 1
@@ -109,6 +151,9 @@ def test_product_deletion(product1, stock_in_warehouse, stock_in_shop,
 @pytest.mark.django_db
 def test_company_deletion(product1, company, warehouse, shop, stock_in_warehouse, stock_in_shop,
                           movement_transfer, movement_inbound, movement_outbound):
+    """
+    Check that the company deletion cascades properly.
+    """
     total, items = company.delete()
     assert total == 10
     assert items['inventory.Company'] == 1
@@ -120,6 +165,9 @@ def test_company_deletion(product1, company, warehouse, shop, stock_in_warehouse
 @pytest.mark.django_db
 def test_location_deletion(product1, company, shop, stock_in_shop, stock_in_warehouse,
                            movement_transfer, movement_inbound, movement_outbound):
+    """
+    Check that the location deletion cascades properly.
+    """
     total, items = shop.delete()
     assert total == 5
     assert items['inventory.Location'] == 1
