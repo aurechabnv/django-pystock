f091b40e3367dbecfa9d4bcd8b90e52a0c9a3577
aurechabnv <git@keltpoint.com>
2025-06-02 17:34:11 +0200

    Add missing / in account urls.py

--- apps/account/urls.py
+++ apps/account/urls.py
@@ -8,10 +8,10 @@ app_name = 'account'
 urlpatterns = [
     path('login/', LoginView.as_view(template_name="account/login.html", next_page="home"), name="login"),
     path('logout/', LogoutView.as_view(next_page="account:login"), name="logout"),
-    path('register', RegisterView.as_view(), name="register"),
-    path('password-reset', PasswordResetView.as_view(template_name="account/password_reset.html"),
+    path('register/', RegisterView.as_view(), name="register"),
+    path('password-reset/', PasswordResetView.as_view(template_name="account/password_reset.html"),
          name="password_reset"),
-    path('password-reset/done', PasswordResetDoneView.as_view(template_name="account/password_reset_done.html"),
+    path('password-reset/done/', PasswordResetDoneView.as_view(template_name="account/password_reset_done.html"),
          name="password_reset_done"),
     path('password-reset-confirm/<uidb64>/<token>/',
          PasswordResetConfirmView.as_view(template_name="account/password_reset_confirm.html"),
