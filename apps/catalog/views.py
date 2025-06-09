dfdbd34d65c3beb51635cfeaf3496f0cde14036a
aurechabnv <git@keltpoint.com>
2025-06-09 15:38:47 +0200

    Require login in catalog and add tests

--- apps/catalog/views.py
+++ apps/catalog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 
@@ -6,7 +6,7 @@ from apps.catalog.forms import ProductForm
 from apps.catalog.models import Product
 
 
-class CatalogView(ListView):
+class CatalogView(LoginRequiredMixin, ListView):
     model = Product
     context_object_name = 'products'
     paginate_by = 5
@@ -14,21 +14,21 @@ class CatalogView(ListView):
     #TODO: add some basic filtering
 
 
-class CatalogCreateView(CreateView):
+class CatalogCreateView(LoginRequiredMixin, CreateView):
     model = Product
     context_object_name = 'product'
     form_class = ProductForm
     success_url = reverse_lazy('product:list')
 
 
-class CatalogUpdateView(UpdateView):
+class CatalogUpdateView(LoginRequiredMixin, UpdateView):
     model = Product
     context_object_name = 'product'
     form_class = ProductForm
     success_url = reverse_lazy('product:list')
 
 
-class CatalogDeleteView(DeleteView):
+class CatalogDeleteView(LoginRequiredMixin, DeleteView):
     model = Product
     context_object_name = 'product'
     success_url = reverse_lazy('product:list')
