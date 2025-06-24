d3ce2205d8f44709c3163cc19026babd8e577d42
aurechabnv <git@keltpoint.com>
2025-06-24 14:54:42 +0200

    Set up sample dashboard

--- pystock/views.py
+++ pystock/views.py
@@ -1,4 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
 
 def home(request):
+    if request.user.is_authenticated:
+        if request.user.is_staff:
+            return redirect('dashboard')
+        else:
+            return redirect('stock:list')
+
     return render(request, 'home.html')
