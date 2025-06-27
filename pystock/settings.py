065dc70cc187976d0e1906a5fade2fecf3961c11
aurechabnv <git@keltpoint.com>
2025-06-27 16:35:16 +0200

    Update static_root

--- pystock/settings.py
+++ pystock/settings.py
@@ -130,11 +130,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATIC_ROOT = BASE_DIR / 'static'
-
-# STATICFILES_DIRS = [
-#     BASE_DIR / 'static',
-# ]
+STATIC_ROOT = BASE_DIR / 'static_files'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
