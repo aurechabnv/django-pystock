e5a9c03b636c1c0fc62fe6a2e313c070c9318d74
aurechabnv <git@keltpoint.com>
2025-06-28 16:26:24 +0200

    Show environment name in HTML header

--- pystock/settings/base.py
+++ pystock/settings/base.py
@@ -78,6 +78,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'pystock.context_processors.export_vars',
             ],
         },
     },
