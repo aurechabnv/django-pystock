cb21d5ad1b0ab27c81e5005b7adfa7e53e41ee3e
aurechabnv <git@keltpoint.com>
2025-06-30 18:40:38 +0200

    Update strict mode

--- pystock/settings/prod.py
+++ pystock/settings/prod.py
@@ -17,7 +17,7 @@ DATABASES = {
         'PORT': '3306',
         'OPTIONS': {
             'charset': 'utf8mb4',
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+            'init_command': "SET sql_mode='STRICT_ALL_TABLES'",
         }
     }
 }
