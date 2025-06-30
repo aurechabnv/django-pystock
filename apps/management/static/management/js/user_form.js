b8779f356f55a34cf964a32b70093bed054c82e4
aurechabnv <2716789+aurechabnv@users.noreply.github.com>
2025-06-30 23:14:34 +0200

    UI fixes (#59)
    * Uniformize spacing and icon size
    
    * Fix companies toggling based on is_staff value

--- apps/management/static/management/js/user_form.js
+++ apps/management/static/management/js/user_form.js
@@ -1,5 +1,14 @@
 $(document).ready(function () {
-    $('#id_is_staff').change(function() {
-        $('#id_companies').parent().toggle();
+    let $id_is_staff = $('#id_is_staff')
+    let $id_companies = $('#id_companies')
+
+    // If `is_staff` is checked, `companies` field must be hidden
+    $id_is_staff.change(function() {
+        $id_companies.parent().css('display', this.checked?'none':'block');
     });
+
+    // Hide `companies` field on initialisation if necessary
+    if ($id_is_staff.is(':checked')) {
+        $id_companies.parent().hide();
+    }
 })
