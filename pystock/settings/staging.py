3913dc0fb69ebe2dace09b5d2b430e835d2acceb
aurechabnv <2716789+aurechabnv@users.noreply.github.com>
2025-06-30 18:45:42 +0200

    Set strict sql mode (#55)

--- pystock/settings/staging.py
+++ pystock/settings/staging.py
@@ -15,5 +15,9 @@ DATABASES = {
         'PASSWORD': env('DATABASE_PASSWORD'),
         'HOST': '127.0.0.1',
         'PORT': '3306',
+        'OPTIONS': {
+            'charset': 'utf8mb4',
+            'init_command': "SET sql_mode='STRICT_ALL_TABLES'",
+        }
     }
 }
